e2246604ad120a7f70dd7f7d725473129cb50586
Kvalitnipreclik <115696189+Kvalitnipreclik@users.noreply.github.com>
2023-04-27 11:04:56 +0200

    Add files via upload

---
+++ kontak.js
@@ -0,0 +1,20 @@
+
+let counter = 0
+function ulozeni(){
+    if(counter === 1){
+        alert("Váš druhý dotaz byl už odeslán! snad už vám je všechno jasné.")
+        
+    } else if( counter === 2){
+        alert("Doufáme, že tohle byl váš poslední dotaz.")
+    }else if( counter >2){
+        alert("Váš dotaz byl zkartován.")
+    }else {  
+    alert("Váš první dotaz byl odeslán ^-^")
+    }
+    counter++
+}
+
+
+function playMyAudio(){
+    document.getElementById("myAudio").play();
+  }
